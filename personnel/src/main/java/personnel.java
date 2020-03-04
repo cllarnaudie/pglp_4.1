afde74ec7fceeeaca4dc57feb0901eefab0f8c4d
Utilisateur <user@debian.arcahe.ovh>
2020-03-04 16:03:40 +0100

    ajout classe personnel.java

---
+++ personnel/src/main/java/personnel.java
@@ -0,0 +1,31 @@
+package main.java;
+
+import java.time.LocalDate; 
+
+/*classe immuable donc final devant classe */
+public final class personnel {
+	
+    private final String nom;
+	private final String prenom; 
+	private final String fonction; 
+	private final LocalDate dateNaissance; 
+	private final ArrayList <NumeroTelephone> numeroTelephone; 
+	
+	public static class Builder() {
+	/*int   dayOfMonth = dateNaissance.getDayOfMonth(); */
+		
+	    private final String nom;
+		private final String prenom; 
+		private final String fonction; 
+		private final LocalDate dateNaissance; 
+		private final ArrayList <NumeroTelephone> numeroTelephone
+	
+	public Builder (){
+		this.nom = ""; 
+		this.prenom = ""; 
+	 
+		
+	}
+	
+}
+}
