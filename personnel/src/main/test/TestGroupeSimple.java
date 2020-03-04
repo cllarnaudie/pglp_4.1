b8d8ec2a998f9b9a2ad943678978b01e821240c1
Utilisateur <user@debian.arcahe.ovh>
2020-03-04 17:48:51 +0100

    ajout classes test

---
+++ personnel/src/main/test/TestGroupeSimple.java
@@ -0,0 +1,14 @@
+package main.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestGroupeSimple {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ personnel/src/main/test/TestPersonnel.java
@@ -0,0 +1,14 @@
+package main.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestPersonnel {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
