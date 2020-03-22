cb0403c72e6746dccb96a189edc2fad61f8c3dd1
Larnaudie <claire.larnaudie@ens.uvsq.fr>
2020-03-22 17:01:42 +0100

    corrections classes test

--- personnel/src/main/test/TestGroupePersonnel.java
+++ personnel/src/main/test/TestGroupePersonnel.java
@@ -1,14 +1,82 @@
 package main.test;
 
-import static org.junit.Assert.*;
+//import static org.junit.Assert.*;
 
 import org.junit.Test;
+import main.java.GroupePersonnel;
+import main.java.Personnel;
 
 public class TestGroupePersonnel {
+	
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGroupePersonnelAjouterPersonne() {
+
+		System.out.println("testGroupePersonnelAjouterPersonne \n");
+
+		GroupePersonnel gp = new GroupePersonnel ();  	
+		Personnel p1 = new Personnel.Builder("Gertrude", "Germaine")
+				       .build(); 
+
+		
+		gp.ajouter_personnel(p1);
+		
+		System.out.println("la personne " + p1.getPrenom() + " " + p1.getNom() + 
+				 " a ete supprime de la liste \n");
+
+	    		     
+}
+	
+	@Test
+	public void testGroupePersonnelSupprimerPersonnel() {
+
+		System.out.println("testGroupePersonnelSupprimerPersonnel \n");
+
+		GroupePersonnel gp2 = new GroupePersonnel ();  	
+		Personnel p2 = new Personnel.Builder("Jean", "Bouysous")
+				        .build(); 
+
+		
+		gp2.ajouter_personnel(p2);
+		gp2.supprimer_personnel(p2);
+	    		     
+}
+	
+	
+
+	@Test
+	public void testGroupeAjouterMemePersonnel() {
+
+		System.out.println("testGroupeAjouterMemePersonnel \n");
+
+		GroupePersonnel gp2 = new GroupePersonnel ();  	
+		Personnel p3 = new Personnel.Builder("Marcel ", "Pagnol ")
+				       .build();
+
+		
+		gp2.ajouter_personnel(p3);
+		gp2.ajouter_personnel(p3);
 	}
+	    	     
+
+
+
+	@Test
+	public void testGroupePersonnelSupprimerPersonneDeuxFois() {
+
+		System.out.println("testGroupePersonnelSupprimerPersonneDeuxFois \n");
+
+		GroupePersonnel gp3 = new GroupePersonnel ();  	
+		Personnel p4 = new Personnel.Builder("More", "Thomas ")
+				       .build();
+		Personnel p5 = new Personnel.Builder("More", "Thomas ")
+			       .build();
+		
+		gp3.ajouter_personnel(p4);
+		gp3.supprimer_personnel(p4);
+		gp3.supprimer_personnel(p4);
+		   	    		     
+}
+	
 
 }
