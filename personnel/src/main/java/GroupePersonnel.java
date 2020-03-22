ebba78fab82c8bb4d38e6b04e448b9a6960c5186
Larnaudie <claire.larnaudie@ens.uvsq.fr>
2020-03-22 17:00:25 +0100

    corrections classes

---
+++ personnel/src/main/java/GroupePersonnel.java
@@ -0,0 +1,52 @@
+package main.java;
+import java.util.ArrayList; 
+import java.util.Iterator;
+
+public  class GroupePersonnel {
+
+	// liste de personnel
+	private ArrayList <Personnel> personnels; 
+	
+	public GroupePersonnel() {
+		personnels = new ArrayList<Personnel>(); 
+	}
+	
+	
+	/**
+	 * Ajouter un personnel à la liste de personnel
+	 * @param personnel
+	 */
+	public void ajouter_personnel(Personnel personnel) {
+		if (personnels.contains(personnel) == true) {
+			System.out.println ("Le membre est déjà dans la liste \n"); 
+		}
+		else {
+			personnels.add(personnel); 
+		}
+	}
+	
+	/**
+	 * Supprimer un personnel de la liste des personnels
+	 * @param personnel
+	 */
+	public void supprimer_personnel(Personnel personnel) {
+		if (personnels.isEmpty() == true) {
+			System.out.println ("La liste est vide \n"); 
+		}
+		else {
+			
+			if (personnels.contains(personnel) == true) {
+				personnels.remove(personnel); 
+				System.out.println ("La personne " + personnel.getPrenom() + " " + 
+				personnel.getNom() + " a ete supprime de la liste \n"); 
+			}
+			
+		}
+	
+	}
+	
+	public Iterator getPersonnels () {
+		return personnels.iterator(); 
+	}
+	
+}
